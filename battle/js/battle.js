2fcd94c9501a262a8c77e99de708dfc9d7fc2c26
oraclepxx <oraclepxx@gmail.com>
2015-02-02 23:04:03 -0800

    -update files

--- battle/js/battle.js
+++ battle/js/battle.js
@@ -66,3 +66,46 @@ var model = {
 }
 
 
+var controller = {
+
+    guesses: 0,
+
+    processGuess: function(input){
+        var result = this.parseGuess();
+        if(result){
+            guesses++;
+            var hit = model.fire(result);
+            if(hit && model.sunk == model.shipNum){
+                view.displayMessage("You win!");
+            }
+        }
+    },
+
+
+    parseGuess: function (input) {
+        var res = "";
+
+        if (input.length != 2) {
+            return null;
+        }
+
+        if (!((input[0] >= "A" && input[0] <= "G") && (input[1] >= 0 && input[1] <= 6))) {
+            return null;
+        }
+
+        var resA = 'A'.charCodeAt('A');
+        res = input[0].charCodeAt('A') - resA;
+        res = res + String((Number(input[1]) + 1));
+
+        return res;
+
+    }
+
+
+};
+
+view.displayHit(controller.parseGuess("A1"));
+view.displayHit(controller.parseGuess("A2"));
+view.displayHit(controller.parseGuess("A3"));
+//view.displayMessage(controller.parseGuess("C3"));
+//alert('B'.charCodeAt('A'));
