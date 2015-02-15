981f590ea143abc8b412b0a4b20faf225b7bbe4e
oraclepxx <oraclepxx@gmail.com>
2015-02-14 16:28:14 -0800

    -add new files

--- handler/handler.js
+++ handler/handler.js
@@ -11,13 +11,24 @@ function init() {
         var loc = "images/" + (i + 1) + ".png";
         imgArray[i].src = loc;
         imgArray[i].onclick = handleImage;
+        imgArray[i].onmousemove = handleMouseMoved;
+        imgArray[i].onmouseover = handleMouseHover;
     }
+    //setTimeout(timer, 3000);
+    //setInterval(timer, 3000);
 
+    //var testImg = document.getElementById("testImg");
+    //testImg.onmousemove = handleMouseMoved;
+}
+
+function timer() {
+    alert("Waiting 3 seconds....");
 }
 
 function handleImage(eventObject) {
     var image = eventObject.target;
     var tmp = image.src;
+    var imgLoc = image.src;
     tmp = tmp.substring(tmp.lastIndexOf("/") + 1, tmp.length - 1);
     tmp = tmp.split(".")[0];
     if (Number(tmp) < 5) {
@@ -27,4 +38,34 @@ function handleImage(eventObject) {
     }
 
     image.src = "images/" + tmp + ".png";
+
+    setTimeout(resetImage, 3000, image, imgLoc);
+}
+
+function resetImage(image, imgLoc) {
+    //var tmp = image.src;
+    //tmp = tmp.substring(tmp.lastIndexOf("/") + 1, tmp.length - 1);
+    //tmp = tmp.split(".")[0];
+    //tmp = tmp - 4;
+    //image.src = "images/" + tmp + ".png";
+    image.src = imgLoc;
+}
+
+
+function handleMouseHover() {
+    alert("Mouse hover");
+}
+
+function handleMouseMoved(eventObject) {
+    var msg = document.getElementById("msg");
+    var clientX = eventObject.clientX;
+    var clientY = eventObject.clientY;
+    var screenX = eventObject.screenX;
+    var screenY = eventObject.screenY;
+    var pageX = eventObject.pageX;
+    var pageY = eventObject.pageY;
+    var message = "Client:" + clientX + "/" + clientY;
+    message = message + "<br/>" + "Screen:" + screenX + "/" + screenY;
+    message = message + "<br/>" + "Page:" + pageX + "/" + pageY;
+    msg.innerHTML = message;
 }
